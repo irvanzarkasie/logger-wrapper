7fcd42598baeb1a0abc71cc657cc71d912fe07f6
irvanzarkasie <irvanzarkasie@gmail.com>
2019-12-21 17:26:33 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="logger-wrapper",
+    version="0.0.1",
+    author="expecc",
+    author_email="expecc@expecc.com",
+    description="Common package for logging purpose",
+    packages=setuptools.find_packages(),
+    include_package_data=True
+)
