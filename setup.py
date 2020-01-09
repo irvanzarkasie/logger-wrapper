2d05672bc88fa990325bb8d82b50319f29112811
irvanzarkasie <irvanzarkasie@gmail.com>
2020-01-09 10:28:47 +0700

    Enhance script for build automation

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 import setuptools, os, subprocess
 
-BUILD_NUMBER = os.path.dirname(os.path.realpath(__file__)) + "/BUILD_NUMBER"
-VERSION_NUMBER = os.path.dirname(os.path.realpath(__file__)) + "/VERSION_NUMBER"
+# Locate config file
+BUILD_NUMBER = os.path.dirname(os.path.realpath(__file__)) + "/build_conf/BUILD_NUMBER"
+VERSION_NUMBER = os.path.dirname(os.path.realpath(__file__)) + "/build_conf/VERSION_NUMBER"
+PACKAGE_NAME = os.path.dirname(os.path.realpath(__file__)) + "/build_conf/PACKAGE_NAME"
 
+# Retrieve config value from config file
 BUILD_NUMBER = subprocess.run(["cat", BUILD_NUMBER], capture_output=True, text=True).stdout.split("\n")[0]
 VERSION_NUMBER = subprocess.run(["cat", VERSION_NUMBER], capture_output=True, text=True).stdout.split("\n")[0]
+PACKAGE_NAME = subprocess.run(["cat", PACKAGE_NAME], capture_output=True, text=True).stdout.split("\n")[0]
 
 setuptools.setup(
     name="logger-wrapper",
